af92dab4ad0e6147d1b8ec6b32d728d25a412a61
AllanMont <allan.mont34@gmail.com>
2024-02-21 13:52:09 +0100

    feat(UserService)Test for all functions

--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -1,11 +1,23 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
+import { User } from '../interfaces/user.interface';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
+  const mockUsers : User[] = [
+    {
+      id: 1,
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john@gmail.com',
+      password: '1234',
+      admin: false,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },]
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -16,7 +28,19 @@ describe('UserService', () => {
     service = TestBed.inject(UserService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should be get User by ID', () => {
+    service.getById('1').subscribe(user => {
+      expect(user).toEqual(mockUsers[0]);
+    });
+  });
+
+  it('should be delete User by ID', () => {
+    service.delete('1').subscribe(user => {
+      expect(user).toEqual(mockUsers[0]);
+    });
+
+    service.getById('1').subscribe(user => {
+      expect(user).toBeUndefined();
+    });
   });
 });
