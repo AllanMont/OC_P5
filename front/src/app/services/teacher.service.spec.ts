d8496e8b39605dbae933ebbd86ead60746fd33b3
AllanMont <allan.mont34@gmail.com>
2024-02-21 13:48:33 +0100

    feat(TeacherService)Tests for all functions

--- front/src/app/services/teacher.service.spec.ts
+++ front/src/app/services/teacher.service.spec.ts
@@ -1,11 +1,29 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
+import { Teacher } from '../interfaces/teacher.interface';
 
 import { TeacherService } from './teacher.service';
 
 describe('TeacherService', () => {
   let service: TeacherService;
+
+  const mockTeachers : Teacher[] = [
+    {
+      id: 1,
+      lastName: 'Doe',
+      firstName: 'John',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      id: 2,
+      lastName: 'Doe',
+      firstName: 'Jane',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ]
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -16,7 +34,16 @@ describe('TeacherService', () => {
     service = TestBed.inject(TeacherService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should get All teachers', () => {
+    service.all().subscribe(teachers => {
+      expect(teachers).toEqual(mockTeachers);
+    });
   });
+
+  it('should get a teacher by id', () => {
+    service.detail('1').subscribe(teacher => {
+      expect(teacher).toEqual(mockTeachers[0]);
+    });
+  });
+  
 });
