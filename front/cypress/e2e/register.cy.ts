6840d416a0bbd7c2b7c5820df2175604b205af01
AllanMont <allan.mont34@gmail.com>
2024-02-22 02:00:00 +0100

    feat(Tests)E2E: Tests E2E for register

---
+++ front/cypress/e2e/register.cy.ts
@@ -0,0 +1,38 @@
+describe('Register spec', () => {
+    it('register successfull', () => {
+      cy.visit('/register')
+  
+      cy.intercept('POST', '/api/auth/register', {
+        body: {
+          email: 'allan.mont34@gmail.com',
+          firstName: 'Allan',
+          lastName: 'Mont',
+          password: 'test!1234',
+        },
+      })
+  
+      cy.intercept('POST', '/api/auth/login', {
+        body: {
+          username: 'yoga@studio.com,',
+          password: 'test!1234',
+        },
+      })
+  
+      cy.intercept(
+        {
+          method: 'GET',
+          url: '/api/session',
+        },
+        []).as('session')
+  
+      cy.get('input[formControlName=email]').type("allan.mont34@gmail.com")
+      cy.get('input[formControlName=firstName]').type("Allan")
+      cy.get('input[formControlName=lastName]').type("Mont")
+      cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+      
+      cy.get('input[formControlName=email]').type("allan.mont34@gmail.com")
+      cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+  
+      cy.url().should('include', '/sessions')
+    })
+  })
