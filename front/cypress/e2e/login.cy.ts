b04d44787d2bcfd9a4fdf20a32843a90f2eeb996
AllanMont <allan.mont34@gmail.com>
2024-02-29 14:37:18 +0100

    feat(E2E)Test E2E for login

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -2,13 +2,6 @@ describe('Login spec', () => {
   it('Login successfull', () => {
     cy.visit('/login')
 
-    cy.intercept('POST', '/api/auth/login', {
-      body: {
-        username: 'yoga@studio.com,',
-        password: 'test!1234',
-      },
-    })
-
     cy.intercept(
       {
         method: 'GET',
@@ -21,4 +14,32 @@ describe('Login spec', () => {
 
     cy.url().should('include', '/sessions')
   })
-});
+
+  it('Login failed', () => {
+    cy.visit('/login')
+
+    cy.get('input[formControlName=email]').type("allan@toto.com")
+    cy.get('input[formControlName=password]').type(`${"allan.1234"}{enter}{enter}`)
+
+    cy.url().should('not.include', '/sessions')
+  })
+
+  it('Logout', () => {
+    cy.visit('/login')
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+    cy.get('.mat-toolbar > .ng-star-inserted > :nth-child(3)').click()
+
+    cy.url().should('not.include', '/sessions')
+  })
+
+  it('Login required fields' , () => {
+    cy.visit('/login')
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+
+    cy.get('.mat-raised-button').should('be.disabled')
+  })
+})
