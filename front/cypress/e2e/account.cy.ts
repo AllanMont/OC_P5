d442e4e69df13562e2270d3bbffa1119f71cbe72
AllanMont <allan.mont34@gmail.com>
2024-02-29 15:21:12 +0100

    feat(E2E)Test E2E for account [done]

--- front/cypress/e2e/account.cy.ts
+++ front/cypress/e2e/account.cy.ts
@@ -42,4 +42,22 @@ describe('Login spec', () => {
 
     cy.get('.mat-raised-button').should('be.disabled')
   })
+
+  it.only('display info user', () => {
+    cy.visit('/login')
+    
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+    
+    cy.get('[routerlink="me"]').click()
+
+    cy.get('.mat-card-content > div.ng-star-inserted > :nth-child(1)').should('contain', 'Admin ADMIN')
+    cy.get('.mat-card-content > div.ng-star-inserted > :nth-child(2)').should('contain', 'yoga@studio.com')
+
+    cy.get('.mat-card-content > div.ng-star-inserted > :nth-child(3)').should('contain', 'admin')
+
+    cy.get('.p2 :nth-child(1)').should('contain', 'February 18, 2024')
+    cy.get('.p2 :nth-child(2)').should('contain', 'February 18, 2024')
+
+  })
 })
